8a82b2d768c3c34e671946ac7202f9dbb4fc6324
Romulo <tp.romulo.domingos@totvspartners.com.br>
2023-11-09 10:07:10 -0300

    Added annotations Mutation and Query

--- src/main/java/com/br/launchersoft/leagueimport/mutation/TeamMutationResolver.java
+++ src/main/java/com/br/launchersoft/leagueimport/mutation/TeamMutationResolver.java
@@ -1,29 +1,30 @@
 package com.br.launchersoft.leagueimport.mutation;
 
-import javax.annotation.Nonnull;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
 
 import com.br.launchersoft.leagueimport.entity.Team;
 import com.br.launchersoft.leagueimport.service.TeamService;
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 
-@Component
+@Controller
 public class TeamMutationResolver implements GraphQLMutationResolver {
 
     @Autowired
     private TeamService teamService;
 
-    public Team createTeam(@Nonnull String name, String address, String areaName, String shortName, String tla) {
+    @MutationMapping
+    public Team createTeam(@Argument String name,@Argument String address, @Argument String areaName,@Argument String shortName,@Argument String tla) {
         return teamService.createTeam(name, address, areaName, shortName, tla);
     }
 
-
+    @MutationMapping
     public Team addPlayerToTeam(Long playerId, Long teamId) {
         return teamService.addPlayerToTeam(playerId, teamId);
     }
-
+    @MutationMapping
     public Team addCoachToTeam(Long coachId, Long teamId) {
         return teamService.addCoachToTeam(coachId, teamId);
     }
--- src/main/java/com/br/launchersoft/leagueimport/resolver/query/TeamQueryResolver.java
+++ src/main/java/com/br/launchersoft/leagueimport/resolver/query/TeamQueryResolver.java
@@ -4,24 +4,29 @@ import com.br.launchersoft.leagueimport.entity.Team;
 import com.br.launchersoft.leagueimport.service.TeamService;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 import java.util.Optional;
 
 import lombok.RequiredArgsConstructor;
 
-@Component
+@Controller
 @RequiredArgsConstructor
 public class TeamQueryResolver implements GraphQLQueryResolver {
 
     private final TeamService teamService;
 
+    @QueryMapping
     public List<Team> getAllTeams() {
         return teamService.getAllTeams();
     }
 
-    public Optional<Team> getTeamById(Long id) {
+    @QueryMapping
+    public Optional<Team> getTeamById(@Argument Long id) {
         return teamService.getTeamById(id);
     }
 }
